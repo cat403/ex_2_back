e302f2f0dd9d657ad4d1ec776884c60ac78397ad
Delian Zidarov <d.zidarov@gmail.com>
2021-12-25 22:54:48 +0900

    worked on the fitness routes

--- routes/fitness.js
+++ routes/fitness.js
@@ -7,7 +7,25 @@ router.get("/", (req, res) => {
   //test.
   res.send("fitness");
 });
-router.get("/:id/calories", (req, res) => {
+router.get("/:id/calories", async (req, res) => {
+  const today = new Date(new Date().toISOString().slice(0, 10));
+
+  try {
+    const user = await User.findById(req.params.id);
+    let calories = user.burnedCalories.map((object) => {
+      if (object.date.valueOf() === today.valueOf()) {
+        return object.caloriesBurned;
+      } else {
+        return 0;
+      }
+    });
+    calories = calories.reduce((previousValue, currentValue) => {
+      return previousValue + currentValue;
+    });
+    res.json({ caloriesBurned: calories });
+  } catch (error) {
+    console.error({ error: "Couldn't find user" });
+  }
   //get todays calories burned
 });
 router.post("/:id/calories", async (req, res) => {
@@ -24,12 +42,44 @@ router.post("/:id/calories", async (req, res) => {
     res.json({ error: "Something went wrong with the save" });
     console.error(error);
   }
-  console.log(userId, calories);
 });
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    res.json({ error: "Couldn't find user" });
+  }
   //get exercises
 });
-router.post("/:id", (req, res) => {
+router.post("/:id", async (req, res) => {
+  const exercise = { ...req.body };
+  delete exercise.routine;
+  const routine = req.body.routine;
+  const user = req.params.id;
+  console.log(exercise);
+  try {
+    const savedExercise = await Fitness.findOneAndUpdate(
+      { user: user },
+      {
+        $push: {
+          savedWorkouts: {
+            name: routine,
+            timer: {
+              repeat: exercise.repeat,
+              exerciseName: exercise.exerciseName,
+              duration: exercise.exerciseTime,
+              break: exercise.breakTime,
+            },
+          },
+        },
+      },
+      { upsert: true, new: true }
+    );
+    console.log(savedExercise);
+  } catch (error) {
+    console.error(error);
+    res.json({ error: "Something went wrong with the save" });
+  }
   // save exercises
 });
 router.delete("/:id", (req, res) => {
